8f8e00cab53a9a9f830d8cb50f1498e66bb99750
Andrew Borisenko <seigiard@gmail.com>
2023-10-28 16:47:40 +0200

    Add OURA request

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const { log } = require('console');
 var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+const Client = require("oura-cloud-api");
+
+const accessToken = 'I5UAHQTSTW76DXV2R3GCDZBDNKXC6CQ6';
+
+http.createServer(async function (req, res) {
+    const client = new Client(accessToken);
+
+    const data = await client.getDailyReadiness({ start_date: "2023-10-25", end_date: "2023-10-28" });
+
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);;
+    res.end(JSON.stringify(data));
     res.end();
 }).listen(process.env.PORT || 3000);
