785ce222339d82f39ab8273f0def1f3f8e16ec1d
Andrew Borisenko <seigiard@gmail.com>
2023-11-02 07:53:09 +0100

    Add HRV Balance

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { log } = require('console');
 var http = require('http');
+const { type } = require('os');
 const Client = require('oura-cloud-api');
 
 const accessToken = 'I5UAHQTSTW76DXV2R3GCDZBDNKXC6CQ6';
@@ -10,23 +11,34 @@ http
   .createServer(async function (req, res) {
     const client = new Client(accessToken);
 
+    const dailyReadinessRawData = await client.getDailyReadiness({
+      start_date: getWeekAgoDate(),
+      end_date: getTodayDate(),
+    });
+
+    const dailySleepRawData = await client.getDailySleep({
+      start_date: getWeekAgoDate(),
+      end_date: getTodayDate(),
+    });
+
     const dataDailyReadiness = getScoreByData(
-      await client.getDailyReadiness({
-        start_date: getWeekAgoDate(),
-        end_date: getTodayDate(),
-      }),
+      dailyReadinessRawData,
       'readiness'
     );
 
-    const dataDailySleep = getScoreByData(
-      await client.getDailySleep({
-        start_date: getWeekAgoDate(),
-        end_date: getTodayDate(),
-      }),
-      'sleep'
+    const dataDailyHrvBalance = getScoreByData(
+      dailyReadinessRawData,
+      'hrv_balance',
+      (item) => item.contributors.hrv_balance
     );
 
-    const data = mergeTwoObject(dataDailyReadiness, dataDailySleep);
+    const dataDailySleep = getScoreByData(dailySleepRawData, 'sleep');
+
+    const data = mergeObjects(
+      dataDailyReadiness,
+      dataDailyHrvBalance,
+      dataDailySleep
+    );
 
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -62,21 +74,45 @@ function getWeekAgoDate() {
   return date.toISOString().slice(0, 10);
 }
 
-function getScoreByData(data, key) {
+function getScoreByData(data, key, field = 'score') {
+  function getValueByField(item) {
+    if (typeof field === 'function') {
+      return field(item);
+    }
+    return item[field];
+  }
+
   return data.data
     .map((item) => ({
       day: item.day,
-      score: item.score,
+      value: getValueByField(item),
     }))
     .reduce((acc, item) => {
-      acc[item.day] = { [key]: item.score };
+      acc[item.day] = { [key]: item.value };
       return acc;
     }, {});
 }
 
-function mergeTwoObject(obj1, obj2) {
-  return Object.keys(obj1).reduce((acc, item) => {
-    acc[item] = { ...obj1[item], ...obj2[item] };
+function mergeObjects(...objs) {
+  const keys = Object.keys(objs[0]);
+
+  return keys.reduce((acc, key) => {
+    acc[key] = objs.reduce((obj, item) => {
+      return { ...obj, ...item[key] };
+    }, {});
     return acc;
   }, {});
 }
+
+/*
+
+"contributors": {
+        "activity_balance": 53,
+        "body_temperature": 100,
+        "hrv_balance": 88,
+        "previous_day_activity": 86,
+        "previous_night": 100,
+        "recovery_index": 91,
+        "resting_heart_rate": 100,
+        "sleep_balance": 80
+      },*/
